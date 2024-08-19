463975ce8b108e703156a40e69e746aca13efcf2
aisuneko icecat <iceneko@protonmail.ch>
2024-08-19 22:09:57 +0800

    feat: major overhaul for report generation; refactored test runner

--- src/aggregator.rs
+++ src/aggregator.rs
@@ -1,8 +1,18 @@
 use crate::utils::Report;
 use serde_json;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{prelude::*, BufWriter};
 
+pub fn generate_report(
+    file_path: String,
+    report: Report,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let report_file = File::create(file_path)?;
+    let mut writer = BufWriter::new(report_file);
+    serde_json::to_writer(&mut writer, &report)?;
+    writer.flush()?;
+    Ok(())
+}
 pub fn aggregate_reports(
     distros: &[&str],
     packages: &[&str],
@@ -12,8 +22,8 @@ pub fn aggregate_reports(
     for &distro in distros {
         for &package in packages {
             let report_path = format!("{}/{}/report.json", distro, package);
-            println!("Aggregating {}", report_path);
             if let Ok(file) = File::open(&report_path) {
+                println!("Aggregating {}", report_path);
                 let mut report: Report = serde_json::from_reader(file)?;
                 report.distro = distro.to_string();
                 consolidated_report.push(report);
--- src/test_runner.rs
+++ src/test_runner.rs
@@ -1,6 +1,7 @@
-use crate::utils::{Report, TempFile};
+use crate::aggregator::generate_report;
+use crate::utils::{CommandOutput, Report, TempFile, TestResult, REMOTE_TMP_DIR};
 use ssh2::Session;
-use std::fs::File;
+use std::fs::{read_to_string, File};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::path::Path;
@@ -31,9 +32,19 @@ impl TestRunner for LocalTestRunner {
         verbose: bool,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let script_path = format!("{}/{}/test.sh", distro, package);
+
+        // get system version and package version
+        let os_version = read_to_string("/proc/version")?;
+        let kernelver_output = Command::new("uname").arg("-r").output()?;
+        let kernel_version = String::from_utf8_lossy(&kernelver_output.stdout).to_string();
+
+        let pkgver_tmpfile = format!("{}/pkgver.tmp", REMOTE_TMP_DIR); // dirty hack for now
         let output = Command::new("bash")
             .arg("-c")
-            .arg(&script_path)
+            .arg(&format!(
+                "mkdir -p {} && source {} && echo $PACKAGE_VERSION > {}",
+                REMOTE_TMP_DIR, script_path, pkgver_tmpfile
+            ))
             .stdout(if verbose {
                 Stdio::inherit()
             } else {
@@ -41,6 +52,33 @@ impl TestRunner for LocalTestRunner {
             })
             .output()?;
 
+        let pkgver_output = Command::new("bash")
+            .arg("-c")
+            .arg(&format!("cat {} && rm {}", pkgver_tmpfile, pkgver_tmpfile))
+            .stdout(Stdio::piped())
+            .output()?;
+        let package_version = String::from_utf8_lossy(&pkgver_output.stdout).to_string();
+
+        // TODO: support multiple tests
+        let all_tests_passed = output.status.success();
+        let test_results: Vec<TestResult> = Vec::from([TestResult {
+            test_name: String::from("test.sh"),
+            passed: output.status.success(),
+        }]);
+
+        let report: Report = Report {
+            distro: distro.to_string(),
+            os_version,
+            kernel_version, // deprecated
+            package_name: package.to_string(),
+            package_type: String::from("package"), // temporarily deprecated
+            package_version,
+            test_results,
+            all_tests_passed,
+        };
+
+        let report_path = format!("{}/{}/report.json", distro, package);
+        generate_report(report_path, report)?;
         if !output.status.success() {
             return Err(format!(
                 "Test failed for {}/{}: {}",
@@ -51,12 +89,7 @@ impl TestRunner for LocalTestRunner {
             .into());
         }
 
-        // 读取并解析 report.json
-        let report_path = format!("{}/{}/report.json", distro, package);
-        let report_file = std::fs::File::open(Path::new(&report_path))?;
-        let report: Report = serde_json::from_reader(report_file)?;
-
-        if !report.all_tests_passed {
+        if !all_tests_passed {
             return Err(format!("Not all tests passed for {}/{}", distro, package).into());
         }
 
@@ -85,6 +118,27 @@ impl RemoteTestRunner {
         if std::env::var("PRINT_SSH_MSG").is_ok() {
             println!("{}", msg);
         }
+    }
+
+    fn run_command(
+        &self,
+        sess: &Session,
+        command: &str,
+    ) -> Result<CommandOutput, Box<dyn std::error::Error>> {
+        let mut channel = sess.channel_session()?;
+        channel.exec(command)?;
+
+        // read output of remote command to prevent deadlocks
+        let mut s = String::new();
+        channel.read_to_string(&mut s)?;
+        channel.send_eof()?;
+        channel.wait_close()?;
+        let command_output = CommandOutput {
+            exit_status: channel.exit_status()?,
+            output: s,
+        };
+        self.print_ssh_msg(&format!("{:?}", command_output));
+        Ok(command_output)
     }
 }
 
@@ -132,8 +186,11 @@ impl TestRunner for RemoteTestRunner {
             local_dir, tar_file
         ));
 
+        // make preparations on the remote server
+        self.run_command(&sess, &format!("mkdir -p {}", REMOTE_TMP_DIR))?;
+
         // 上传压缩文件到远程服务器
-        let remote_tar_path = format!("/tmp/lintestor/{}", tar_file);
+        let remote_tar_path = format!("{}/{}", REMOTE_TMP_DIR, tar_file);
         let mut remote_file = sess.scp_send(
             Path::new(&remote_tar_path),
             0o644,
@@ -150,64 +207,105 @@ impl TestRunner for RemoteTestRunner {
         drop(remote_file);
 
         // 清理远程目录，解压文件并在远程服务器上运行测试
-        let remote_dir = format!("/tmp/lintestor/{}", package);
-        let mut channel = sess.channel_session()?;
-
-        channel.exec(&format!(
-            "rm -rf {}; mkdir -p {} && tar xzf {} -C {} --overwrite",
-            remote_dir, remote_dir, tar_file, remote_dir
-        ))?;
+        let remote_dir = format!("{}/{}", REMOTE_TMP_DIR, package);
         self.print_ssh_msg(&format!(
             "Extracting file {} on remote server at {}",
             tar_file, remote_dir
         ));
-
-        // 读取远程命令的输出以防止死锁
-        let mut s = String::new();
-        channel.read_to_string(&mut s)?;
-        self.print_ssh_msg(&format!("Command output: {}", s));
-
-        channel.send_eof()?;
-        channel.wait_close()?;
-        let exit_status = channel.exit_status()?;
-        if exit_status != 0 {
+        if let Ok(CommandOutput {
+            exit_status: 0,
+            output: _,
+        }) = self.run_command(
+            &sess,
+            &format!(
+                "rm -rf {}; mkdir -p {} && tar xzf {} -C {} --overwrite",
+                remote_dir, remote_dir, remote_tar_path, remote_dir
+            ),
+        ) {
+            self.print_ssh_msg(&format!(
+                "Successfully extracted file {} on remote server at {}",
+                tar_file, remote_dir
+            ));
+        } else {
             return Err("Failed to extract test files on remote server".into());
         }
 
         // 运行测试命令
-        let mut channel = sess.channel_session()?;
-        channel.exec(&format!("cd {} && ./test.sh", remote_dir))?;
         self.print_ssh_msg(&format!("Running tests in directory {}", remote_dir));
-        let mut s = String::new();
-        channel.read_to_string(&mut s)?;
-        self.print_ssh_msg(&format!("Test command output: {}", s));
-        channel.send_eof()?;
-        channel.wait_close()?;
-        let exit_status = channel.exit_status()?;
-        if exit_status != 0 {
-            return Err(format!("Test failed for {}/{}: {}", distro, package, s).into());
+
+        let script_path = format!("{}/test.sh", remote_dir);
+        let pkgver_tmpfile = format!("{}/pkgver.tmp", REMOTE_TMP_DIR);
+        let result = self.run_command(
+            &sess,
+            &format!(
+                "source {} && echo $PACKAGE_VERSION > {}",
+                script_path, &pkgver_tmpfile
+            ),
+        );
+
+        // get system version and package version
+        let os_version = self.run_command(&sess, "cat /proc/version")?;
+        let package_version = self.run_command(
+            &sess,
+            &format!("cat {} && rm {}", pkgver_tmpfile, pkgver_tmpfile),
+        )?;
+
+        let kernel_version = self.run_command(&sess, "uname -r")?;
+
+        let mut test_passed = false;
+
+        match result {
+            Ok(i) => {
+                if i.exit_status == 0 {
+                    test_passed = true;
+                    self.print_ssh_msg(&format!("Test successful for {}/{}", distro, package));
+                } else {
+                    self.print_ssh_msg(&format!(
+                        "Test failed for {}/{}: {}",
+                        distro, package, i.output
+                    ));
+                }
+            }
+            Err(e) => return Err(format!("Test failed for {}/{}: {}", distro, package, e).into()),
         }
+        // TODO: support multiple tests
+        let test_results: Vec<TestResult> = Vec::from([TestResult {
+            test_name: String::from("test.sh"),
+            passed: test_passed,
+        }]);
+        let report: Report = Report {
+            distro: distro.to_string(),
+            os_version: os_version.output,
+            kernel_version: kernel_version.output, // deprecated
+            package_name: package.to_string(),
+            package_type: String::from("package"), // temporarily deprecated
+            package_version: package_version.output,
+            test_results,
+            all_tests_passed: test_passed,
+        };
 
         // 压缩远程测试目录
-        let remote_tar_file = format!("/tmp/lintestor/{}_result.tar.gz", package);
-        let mut channel = sess.channel_session()?;
-        channel.exec(&format!(
-            "cd /tmp/lintestor && tar czf {} -C {} . --overwrite",
-            remote_tar_file, package
-        ))?;
+        let remote_tar_file = format!("{}/{}_result.tar.gz", REMOTE_TMP_DIR, package);
         self.print_ssh_msg(&format!(
             "Compressing remote directory {} into {}",
             remote_dir, remote_tar_file
         ));
 
-        // 读取远程命令的输出以防止死锁
-        let mut s = String::new();
-        channel.read_to_string(&mut s)?;
-        self.print_ssh_msg(&format!("Command output: {}", s));
-        channel.send_eof()?;
-        channel.wait_close()?;
-        let exit_status = channel.exit_status()?;
-        if exit_status != 0 {
+        if let Ok(CommandOutput {
+            exit_status: 0,
+            output: _,
+        }) = self.run_command(
+            &sess,
+            &format!(
+                "cd {} && tar czf {} -C {} . --overwrite",
+                REMOTE_TMP_DIR, remote_tar_file, package
+            ),
+        ) {
+            self.print_ssh_msg(&format!(
+                "Successfully compressed remote test result at {} into {}",
+                remote_dir, remote_tar_file
+            ));
+        } else {
             return Err("Failed to compress test results on remote server".into());
         }
 
@@ -238,6 +336,8 @@ impl TestRunner for RemoteTestRunner {
 
         // 下载测试报告
         let report_path = format!("{}/report.json", local_dir);
+        generate_report(report_path.clone(), report)?;
+
         let report_file_path = Path::new(&report_path);
         if report_file_path.exists() {
             let mut file = File::open(&report_path)?;
