1abb22247542dc6dbde9350d2a2cdfc03c9d4fb4
255doesnotexist <1293883574jcy@gmail.com>
2024-08-26 03:46:47 +0800

    feat: a test script manager

---
+++ src/testscript_manager.rs
@@ -0,0 +1,28 @@
+pub struct TestScriptManager {
+    distro: String,
+    package: String,
+    test_scripts: Vec<String>,
+}
+
+impl TestScriptManager {
+    pub fn new(distro: &str, package: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let dir = format!("./{}/{}", distro, package);
+        let mut test_scripts = Vec::new();
+        for entry in std::fs::read_dir(&dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_file() && path.extension().unwrap_or_default() == "sh" {
+                test_scripts.push(path.to_str().unwrap_or_default().to_string());
+            }
+        }
+        Ok(TestScriptManager {
+            distro: distro.to_string(),
+            package: package.to_string(),
+            test_scripts,
+        })
+    }
+
+    pub fn get_test_scripts(&self) -> &[String] {
+        &self.test_scripts
+    }
+}
