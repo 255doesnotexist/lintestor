bb284190794def8fc6879a61a485ab2c449e7da6
255doesnotexist <1293883574jcy@gmail.com>
2024-08-30 10:57:26 +0800

    feat: add testing_type to distro testing configs, skip qemu startup if testing_type is just locally or remote (skipping logic is going to be added)

--- src/config.rs
+++ src/config.rs
@@ -9,13 +9,24 @@ pub struct Config {
 
 #[derive(Debug, Deserialize)]
 pub struct DistroConfig {
+    pub testing_type: String, // 'locally' or 'remote' or 'qemu-based-remote'
     #[serde(rename = "startup_script")]
-    pub startup_script: String,
+    #[serde(default, skip_serializing_if = "is_not_qemu_based_remote")]
+    pub startup_script: String, 
+
     #[serde(rename = "stop_script")]
+    #[serde(default, skip_serializing_if = "is_not_qemu_based_remote")]
     pub stop_script: String,
+
     #[serde(rename = "connection")]
     pub connection: ConnectionConfig,
     pub skip_packages: Option<Vec<String>>,
+}
+
+impl DistroConfig {
+    fn is_not_qemu_based_remote(&self) -> bool {
+        self.testing_type != "qemu-based-remote"
+    }
 }
 
 #[derive(Clone, Debug, Deserialize)]
