f0e389fb97ba76cd4143243388b778b394feb9da
aisuneko icecat <iceneko@protonmail.ch>
2024-10-20 15:39:24 +0800

    testing: add test files for integration test

---
+++ tests/test_files/distro_a/test3/test.sh
@@ -0,0 +1,6 @@
+echo "This is yet another example dummy test script..."
+
+# Do your stuff here
+sleep 0
+
+return 0
---
+++ tests/test_files/distro_b/test1/test3.sh
@@ -0,0 +1,6 @@
+echo "This is an example dummy test script but is the third one..."
+
+# Do your stuff here
+sleep 0
+
+return 0
