4d488aa24b58307477264434cebd5c71dfa14adb
255doesnotexist <1293883574jcy@gmail.com>
2024-11-18 18:55:06 +0800

    tests(fedora): initial test envs ready
    - start qemu & stop qemu scripts ok
    - test config ok

---
+++ fedora/start_ssh.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# the following environmental variables should be controlled directly from TestEnvManager
+# USER="root"
+# PASSWORD="riscv"
+# PORT=2222
+# ADDRESS="localhost"
+
+# echo $USER
+# echo $PASSWORD
+# echo $PORT
+# echo $ADDRESS
+
+# Function to check if sshpass is installed
+check_sshpass() {
+    if ! command -v sshpass &> /dev/null; then
+        echo "sshpass is not installed. Please install it to proceed."
+        exit 1
+    fi
+}
+
+# Function to check if SSH connection is possible
+check_ssh_connection() {
+    if ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 -i ~/.ssh/id_rsa "$USER@$ADDRESS" -p "$PORT" "exit" &> /dev/null; then
+        echo "Able to establish SSH connection."
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Function to wait for environment to start and become accessible via SSH
+wait_for_connection() {
+    local retries=200 # 不知道为什么这个 fedora 开机很慢 
+    local delay=5
+
+    for ((i=0; i<$retries; i++)); do
+        if check_ssh_connection; then
+            echo "Environment is accessible via SSH."
+            return 0
+        fi
+        echo "Environment not connected yet. Retrying in $delay seconds..."
+        sleep $delay
+    done
+
+    echo "Failed to connect to environment via SSH."
+    exit 1
+}
+
+# Main script execution starts here
+
+# Check if sshpass is installed
+check_sshpass
+
+# Check if SSH connection is possible
+if check_ssh_connection; then
+    echo "Already connected to environment via SSH. Exiting..."
+    exit 0
+fi
+
+
+# Wait for environment to become accessible via SSH
+wait_for_connection
+
+exit 0
---
+++ fedora/stop_ssh.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define the SSH connection parameters
+# USER="root"
+# PASSWORD="root"
+# PORT=2222
+# ADDRESS="localhost"
+
+# Function to check if sshpass is installed
+check_sshpass() {
+    if ! command -v sshpass &> /dev/null; then
+        echo "sshpass is not installed. Please install it to proceed."
+        exit 1
+    fi
+}
+
+# Function to stop environment via SSH
+stop_environment_ssh() {
+    local retries=3
+    local delay=5
+
+    for ((i=0; i<$retries; i++)); do
+        if ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 -i ~/.ssh/id_rsa "$USER@$ADDRESS" -p "$PORT" "halt" &> /dev/null; then
+            echo "Successfully sent halt command to environment via SSH."
+            return 0
+        fi
+        echo "Failed to send halt command. Retrying in $delay seconds..."
+        sleep $delay
+    done
+
+    echo "Failed to stop environment via SSH after multiple attempts."
+    return 1
+}
+
+
+# Main script execution starts here
+
+# Check if sshpass is installed
+check_sshpass
+
+stop_environment_ssh
+
+# Exit the script
+exit 0
