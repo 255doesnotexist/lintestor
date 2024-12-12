447c3da691ad98f7775ea226a551f7ac663b712e
255doesnotexist <1293883574jcy@gmail.com>
2024-12-12 09:35:25 +0800

    fedora: specified -bios for old qemu
    - to fix the stucking ci

--- fedora/boot.sh
+++ fedora/boot.sh
@@ -5,6 +5,7 @@ qemu-system-riscv64 -nographic \
     -m 4G \
     -smp 4 \
     -drive file=fedora-disk-gcc.raw,format=raw,if=virtio \
+    -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.bin \
     -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf \
     -netdev user,id=net0,hostfwd=tcp::2223-:22 \
     -device virtio-net-device,netdev=net0
